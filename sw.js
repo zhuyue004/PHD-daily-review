99bb667c21147030c429c1cb7912c6badb002398
zhuyue004 <zephyryue95@gmail.com>
2026-07-30 11:59:40 +0800

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,1 @@
+self.addEventListener('install',e=>e.waitUntil(caches.open('phd-review-v1').then(c=>c.addAll(['./','./index.html','./style.css','./app.js','./manifest.webmanifest','./icon.svg']))));self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
